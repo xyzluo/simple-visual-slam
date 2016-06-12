467103acc2edd2369b55bfb973860d373c2d25a1
quantumabacus <quantumabacus1@gmail.com>
2016-06-12 16:17:18 -0500

    moved from bitbucket

---
+++ bundle_adjustment/include/mappoint.h
@@ -0,0 +1,31 @@
+#ifndef MAPPOINT_H
+#define MAPPOINT_H
+
+#include <eigen3/Eigen/Core>
+
+namespace vslam{
+namespace ba{
+	
+class MapPoint{
+public:
+	EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
+	MapPoint(const Eigen::Vector3d& position, int id, bool fixed = false);
+	const Eigen::Vector3d& getPosition();
+	void setPosition(const Eigen::Vector3d& position);
+	int getId();
+	void setId(int id);
+	void setFixed();
+	bool isFixed();
+	void addDeltaPosition(const Eigen::Vector3d& delta_position);
+	
+	int state_index_;
+private:
+	Eigen::Vector3d position_; 
+	int id_;
+	bool fixed_; 	
+}; //class MapPoint
+	
+} //namespace ba
+} //namespace vslam
+
+#endif
